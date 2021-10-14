b7e645396db3ebdd232f41dac3d66cc7ce37c3c4
NguyenDinh07 <91287570+NguyenDinh07@users.noreply.github.com>
2021-10-15 02:11:57 +0700

    Create Insertanodeattheheadofalinkedlist.java

---
+++ DSA_20020304/src/Week4/Insertanodeattheheadofalinkedlist.java
@@ -0,0 +1,23 @@
+public class Insertanodeattheheadofalinkedlist {
+
+
+    // Complete the insertNodeAtHead function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        newNode.next = llist;
+        llist = newNode;
+        return llist;
+    }
+
+
+}
